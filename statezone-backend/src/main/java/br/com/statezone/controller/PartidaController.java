548fcc8975ab777ff122479e800572f6d0639e47
guiantebii <guiantebi2000@gmail.com>
2026-05-26 19:45:53 -0300

    feat: implement player service layer with CRUD and validations

---
+++ statezone-backend/src/main/java/br/com/statezone/controller/PartidaController.java
@@ -0,0 +1,48 @@
+package br.com.statezone.controller;
+
+import br.com.statezone.dto.PartidaRequestDto;
+import br.com.statezone.dto.PartidaResponseDto;
+import br.com.statezone.service.PartidaService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+@RequestMapping("/partidas")
+@RequiredArgsConstructor
+public class PartidaController {
+    private final PartidaService partidaService;
+
+    @PostMapping
+    public ResponseEntity<PartidaResponseDto> criarPartida(@RequestBody @Valid PartidaRequestDto dto){
+        PartidaResponseDto response = partidaService.criar(dto);
+        URI uri = URI.create("/partidas/" + response.id());
+
+        return ResponseEntity
+                .created(uri)
+                .body(response);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PartidaResponseDto>> listarTodasPartidas(){
+        return ResponseEntity.ok(partidaService.listarTodas());
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<PartidaResponseDto> obterPartidaPorId(@PathVariable Long id){
+        return ResponseEntity.ok(partidaService.buscarPorId(id));
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<PartidaResponseDto> atualizarPartida(@RequestBody @Valid PartidaRequestDto dto,@PathVariable Long id){
+        return ResponseEntity.ok(partidaService.atualizar(dto,id));
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarPartida(@PathVariable Long id){
+        partidaService.deletar(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
---
+++ statezone-backend/src/main/java/br/com/statezone/dto/PartidaResponseDto.java
@@ -0,0 +1,30 @@
+package br.com.statezone.dto;
+
+import br.com.statezone.enums.StatusPartida;
+
+import java.time.LocalDateTime;
+
+public record PartidaResponseDto(
+        Long id,
+        String estadio,
+        String arbitro,
+        Integer rodada,
+        LocalDateTime dataPartida,
+        StatusPartida status,
+
+        Integer golsMandante,
+        Integer golsVisitante,
+
+        Long campeonatoId,
+        String campeonatoNome,
+
+        Long timeMandanteId,
+        String timeMandanteNome,
+
+        Long timeVisitanteId,
+        String timeVisitanteNome,
+
+        LocalDateTime criadoEm,
+        LocalDateTime atualizadoEm
+        ) {
+}
